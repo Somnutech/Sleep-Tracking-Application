49a7fffdb7e603faf52195e8a57915799025fab6
CAubert <c4meronba@gmail.com>
2015-10-01 14:14:06 -0700

    Initial Commit

---
+++ Sleep Tracking Application/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Sleep Tracking Application
+//
+//  Created by Cameron Aubert on 10/1/15.
+//  Copyright © 2015 Cameron Aubert. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
